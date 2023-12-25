a1b6cc386ab64d24d668e882a7923bdea5d07b6f
Wasik Ahmed Apon <22-47698-2@student.aiub.edu>
2023-12-25 20:49:23 +0600

    Enhanced book route: Added endpoint for updating book details by ID (#3)
    This message concisely captures the essence of the modification made in the booksRoute.js file by introducing an endpoint that facilitates the update of book details based on a specified book ID. It builds upon the existing CRUD functionality to incorporate the ability to modify book information.

--- backend/routes/booksRoute.js
+++ backend/routes/booksRoute.js
@@ -14,9 +14,22 @@ router.get("/", async (req, res) => {
         });
     } catch(err) {
         console.log(err.message);
-        res.status(500).json({ message: err.message });
+        res.status(500).send({ message: err.message });
     }
 });
+
+// Route for Get One Book by id
+router.get("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const book = await Book.findById(id);
+
+        return res.status(200).json(book);
+    } catch(err) {
+        console.log(err.message);
+        res.status(500).send({ message: err.message })
+    }
+})
 
 // Router for Save a new Book
 router.post("/", async (req, res) => {
@@ -40,8 +53,36 @@ router.post("/", async (req, res) => {
 
         return res.status(201).send(book);
     } catch(error) {
-        res.status(400).json({ message: error.message });
+        res.status(400).send({ message: error.message });
         console.log(error);
+    }
+});
+
+// Route for Update a Book
+router.put("/:id", async (req, res) => {
+    try {
+        if(
+            !req.body.title || 
+            !req.body.author || 
+            !req.body.publishYear
+        ) {
+            return res.status(400).send({
+                message: "Send all required fields: title, author, publishYear"
+            })
+        }
+
+        const { id } = req.params
+        const result = await Book.findByIdAndUpdate(id, req.body);
+
+        if(!result) {
+            return res.status(404).json({ message: "Book not found" });
+        }
+
+        return res.status(200).send({ message: "Book updated successfully" })
+
+    } catch(err) {
+        console.log(err.message);
+        res.status(500).send(500).send({ message: err.message });
     }
 });
 
