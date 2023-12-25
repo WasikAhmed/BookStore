3ffbdf1fac2ee949ec8da0dd352d8ca24ac628e8
Wasik Ahmed Apon <22-47698-2@student.aiub.edu>
2023-12-25 19:14:52 +0600

    Added Book model, route for CRUD operations, and database connection (#2)
    This message encapsulates the changes made in the commit:
    - The addition of a Book model in bookModel.js with its defined schema.
    - The creation of a route in booksRoute.js to handle GET and POST requests for book data.
    - Establishing the connection to the database in server.js using Mongoose.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
 require("dotenv").config()
+const booksRoute = require("./routes/booksRoute")
 
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
+
+mongoose.connect(DATABASE_URL)
+    .then(() => {
+        console.log("Connected to Database");
+    })
+    .catch((error) => {
+        console.log(error)
+    });
+
+app.use(express.json());
+app.use('/books', booksRoute);
 
 app.listen(PORT, () => {
     console.log(`Server Started at http://localhost:${PORT}`)
